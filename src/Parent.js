f6ff4c5389413c96ce90170bfbd7c0e6d006a61c
asuthamm <asuthamm@gmail.com>
2020-09-19 14:13:24 -0400

    final

--- src/Parent.js
+++ src/Parent.js
@@ -4,18 +4,30 @@ import Child from './Child'
 
 class Parent extends Component {
 
-  constructor() {
-    super()
-    this.state = {
-      color: getRandomColor()
-    }
+  state = {
+    color: getRandomColor(),
+    childrenColor: '#FFF'
+  }
+
+  tickle = (e) => {
+    // console.log(e.target)
+    // console.log('Tee hee!')
+    console.log(this.props)
+  }
+
+  changeColor = (newChildColor) => {
+    this.setState({
+      color: getRandomColor(),
+      childrenColor: newChildColor
+    })
   }
 
   render() {
     return (
       <div className="parent" style={{backgroundColor: this.state.color}}>
-        <Child />
-        <Child />
+        <button onClick={this.tickle}>Tickle me!</button>
+        <Child color={this.state.childrenColor} handleColorChange={this.changeColor}/>
+        <Child color={this.state.childrenColor} handleColorChange={this.changeColor}/>
       </div>
     )
   }
